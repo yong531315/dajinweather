b35d81d2ed5a4bb9318ccf2c4aa58efb1ec3799b
dajin <yong531315@gmail.com>
2014-11-12 13:49:00 +0800

    second

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -4,12 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private TextView tv_banner;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		tv_banner=(TextView) findViewById(R.id.tv_banner);
+		
 	}
 }
